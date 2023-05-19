9ec63ee86b42a904a554baa6c8f86e30b85cb28b
Guilherme Jesuino Ramires <guilherme.ramires@quintoandar.com.br>
2023-05-19 16:48:00 -0300

    ✨feature: Improves theme & animations

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -1,8 +1,7 @@
 extends Control
 
-
-const NORMAL_COLOR: Color = Color.WHITE
-const HOVER_COLOR: Color = Color(Color.WHITE, 0.5)
+const CREATOR_INSTAGRAM_URL = "https://www.instagram.com/ramirestech.games/"
+const CREATOR_LINKEDIN_URL = "https://www.linkedin.com/in/guilherme-ramires-4480a0160/"
 
 
 @onready var start_button: TextureButton = $MarginContainer/VBoxContainer/StartButton
@@ -14,22 +13,6 @@ func _ready() -> void:
 	MusicPlayer.play_random_music()
 
 
-func _on_start_button_mouse_entered() -> void:
-	start_button.modulate = HOVER_COLOR
-
-
-func _on_start_button_mouse_exited() -> void:
-	start_button.modulate = NORMAL_COLOR
-
-
-func _on_quit_button_mouse_entered() -> void:
-	quit_button.modulate = HOVER_COLOR
-
-
-func _on_quit_button_mouse_exited() -> void:
-	quit_button.modulate = NORMAL_COLOR
-
-
 func _on_quit_button_button_up() -> void:
 	get_tree().quit()
 
@@ -39,3 +22,11 @@ func _on_start_button_button_up() -> void:
 	transition.play_in()
 	await transition.animation.animation_finished
 	get_tree().change_scene_to_file(Game.PLAYER_REGISTER_SCREEN)
+
+
+func _on_instagram_button_up() -> void:
+	OS.shell_open(CREATOR_INSTAGRAM_URL)
+
+
+func _on_linkedin_button_up() -> void:
+	OS.shell_open(CREATOR_LINKEDIN_URL)
--- scripts/players_register.gd
+++ scripts/players_register.gd
@@ -1,7 +1,5 @@
 extends Control
 
-const NORMAL_COLOR: Color = Color.WHITE
-const HOVER_COLOR: Color = Color(Color.WHITE, 0.5)
 
 @onready var player_one_name: LineEdit = $MarginContainer/VBoxContainer/VBoxContainer/LineEdit
 @onready var player_two_name: LineEdit = $MarginContainer/VBoxContainer/VBoxContainer2/LineEdit
@@ -26,11 +24,3 @@ func _on_play_button_button_up() -> void:
 	transition.play_in()
 	await transition.animation.animation_finished
 	get_tree().change_scene_to_file(Game.MATCH_SCREEN)
-
-
-func _on_play_button_mouse_entered() -> void:
-	play_button.modulate = HOVER_COLOR
-
-
-func _on_play_button_mouse_exited() -> void:
-	play_button.modulate = NORMAL_COLOR
